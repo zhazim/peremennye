a5e93023aa38931fc7942de3a39e4ae89613912f
Azim <zasynovazim@gmail.com>
2021-09-08 17:32:04 +0600

    peremennye

---
+++ js/main.js
@@ -0,0 +1,153 @@
+// Task #1.
+// Переменные и типы данных
+
+// Задание №1
+// 	Вывести в консоль "Hello World"
+
+
+
+
+//llllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllll
+console.log("Hello World");
+//llllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllll
+
+
+
+// Задание №2
+// 	Создать переменную. Поместить в
+// нее строку "Hello".
+// Создать 2ую переменную со значением
+// "World". Вывести alert "Hello World"
+// при помощи переменных.
+
+
+
+
+
+//llllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllll
+// let name = "Hello";
+// let name2 = "World";
+// alert(name + name2);
+//llllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllll
+
+
+
+
+
+
+// Задание №3
+// 	Создать переменные со всеми типами
+// данных.
+
+
+
+
+
+//llllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllll
+// let num = 3;
+// let string = "строка";
+// let boolean = true;   //false
+// let zero = null;
+// let undefined = undefined;
+//llllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllll
+
+
+
+
+// Задание №4
+// Определите переменные str, num, flag и
+// txt со значениями «Привет», 123, true,
+// «true». При помощи оператора
+// определения типа убедитесь,
+// что переменные  принадлежат типам:
+// string, number, boolean.
+
+
+
+
+//llllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllll
+// let str = "Привет";
+// let num = 123;
+// let flag = true;
+// let txt = true;
+
+// console.log(typeof str);
+// console.log(typeof num);
+// console.log(typeof flag);
+// console.log(typeof txt);
+//llllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllll
+
+
+
+
+// Задание №5
+// Создайте программу на JavaScript, которая будет запрашивать у
+// пользователя имя и профессию, а затем выводить на экран сообщение,
+// которое будет сообщать пользователю эту информацию вместе:
+// Пример ввода:
+// 	•	Sam
+// 	•	Programmer
+// Пример вывода:
+// 	•	Hello, Sam! You work as a Programmer!
+
+
+
+
+
+//llllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllll
+// let name3 = prompt("Как тебя зовут,");
+// let profi = prompt("Какая у тебя профессия?");
+// alert(`Привет ${name3}. Ты ${profi}, отлично`);
+//llllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllll
+
+
+
+
+
+// Указания:
+// Вывод разных подсчетов может быть в разных alert-окнах.
+// Дополнительные баллы за вывод сообщения в одном окне.
+
+
+
+
+
+// lllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllll
+
+// Экстра таск #1
+
+// Задание №1
+// Создайте переменные a1, a2, a3, a4, a5.
+// При помощи математических операторов
+// (сложение, вычитание и т.д.)
+// найдите значения выражений:
+// 5 + 3,
+// 5 - 3,
+// 5 * 3,
+// 5 / 3,
+// поместив результат каждого выражения в соответствующую переменную.
+// Например:
+// let a1 = 5 + 3.
+
+
+// lllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllll
+
+//  Я НЕМНОЖКО НЕ ПОНЯЛ ЗАДАЧУ. НАДЕЮСЬ Я ПРАВИЛЬНО ПОНЯЛ И ПРАВИЛЬНО СДЕЛАЛ.
+
+// var per = "h1";
+// var per2 = "h2";
+// var per3 = "h3";
+// var per4 = "h4";
+// var per5 = "h5";
+// per = 5 + 3;
+// per2 = 5 - 3;
+// per3 = 5 * 3;
+// per4 = 5 / 3;
+// per5 = 5 % 3;
+// console.log(per);
+// console.log(per2);
+// console.log(per3);
+// console.log(per4);
+// console.log(per5);
+// console.log(per, per2, per3, per4, per5);
+// llllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllll
